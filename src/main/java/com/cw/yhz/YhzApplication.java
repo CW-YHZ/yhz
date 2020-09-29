f4be8adc6687da9b80bfb64978302db9e7c1330b
cw <2318331390@qq.com>
2020-09-29 10:02:26 +0800

    Initial commit

---
+++ src/main/java/com/cw/yhz/YhzApplication.java
@@ -0,0 +1,13 @@
+package com.cw.yhz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhzApplication.class, args);
+    }
+
+}
